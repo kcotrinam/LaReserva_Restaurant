865f5d90fbb5a3706b084e4d318897de47b83b32
kevin Cotrina <kcotrinam92@gmail.com>
2021-02-01 00:43:28 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# LaReserva_Restaurant
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Index from './components/main';
-import './scss/styles.scss';
-
-const container = document.querySelector('.content');
-const renderHome = new Index(container);
-renderHome.renderPage();
